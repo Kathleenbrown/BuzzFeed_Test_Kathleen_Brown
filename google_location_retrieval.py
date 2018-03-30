76aae00d0a36f0491f868b648c2cf16394b129e9
Kathleen Brown <kathleen.brown@mines.sdsmt.edu>
2018-03-30 14:04:10 -0600

    Initial Commit: Adding Documentation
    Adding DocString Documentation to add information about the problem, detail to-dos and add comments.

---
+++ google_location_retrieval.py
@@ -0,0 +1,39 @@
+"""Google_Location_Retrieval
+
+Author: Kathleen Brown
+Prepared For: BuzzFeed Recruiting Team
+Date: 03/30/2018
+
+Problem:
+    Using the Google Maps API, retrieve the JSON response after
+    applying a query parameter filter to the geocode endpoint. Then take
+    the reponse and transform it to produce the expected output:
+
+         Green Bay
+
+         ID: <insert place_id here>
+
+         Latitude: <insert latitude here>
+
+         Longitude: <insert longitude here>
+
+
+    Starting Endpoint: http://maps.googleapis.com/maps/api/geocode/json
+
+
+Source Control:
+    url = https://github.com/Kathleenbrown/BuzzFeed_Test_Kathleen_Brown
+    status = private ( please reach out to me to access this, I just didn't
+                want to have my code accessable by other possible
+                participants )
+
+Comments:
+    * Hooking project to source control for my own benefit as well as
+        offering recruiters the chance to see how my code develops
+    
+Todos:
+    * Sign-in to Google Console and retrieve API key
+    * Connect to the API using the given link to retrieve JSON object
+    * Parse returned JSON object to give desired output
+    
+"""
